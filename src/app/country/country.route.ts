467a7ec13b73c3e0d98b619928c58169dd0a7a7f
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-08-13 23:43:43 -0400

    feat: add layout-component and routes

--- src/app/country/country.route.ts
+++ src/app/country/country.route.ts
@@ -1,10 +1,21 @@
+import { CountryLayoutComponent } from './layout/CountryLayout/CountryLayout.component';
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 
 export const countryRoutes: Routes = [
     {
         path: '',
-        component: ByCapitalPageComponent,
+        component: CountryLayoutComponent,
+        children: [
+            {
+                path: 'by-capital',
+                component: ByCapitalPageComponent
+            },
+            {
+                path: '**',
+                redirectTo: 'by-capital'
+            }
+        ]
     },
 //     {
 //         path: 'country',
