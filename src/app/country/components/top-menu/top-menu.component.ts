d187a85668f58527d9c38afe412187ff3c042e63
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-03-26 23:15:57 -0300

    add top menu

---
+++ src/app/country/components/top-menu/top-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'country-top-menu',
+  imports: [],
+  templateUrl: './top-menu.component.html',
+
+})
+export class TopMenuComponent { }
