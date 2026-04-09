aee52c3c9ed0e7912215fd7361048dc1d5e2a264
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-04-08 23:38:06 -0400

    add inputs ycomponets

---
+++ src/app/country/components/search-input/search-input.component.ts
@@ -0,0 +1,17 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+
+@Component({
+  selector: 'country-search-input',
+  imports: [],
+  templateUrl: './search-input.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SearchInputComponent {
+  placeholder = input('Buscar');
+  value = output<string>();
+}
