11feac789d7998d63335853bb6033dfd369d1e43
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-05-29 00:35:44 -0400

    feat: add country information page

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -4,10 +4,15 @@ import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
 import { ByCapitalPageComponent } from '../by-capital-page/by-capital-page.component';
 import { NotFoundComponent } from '../../../shared/components/not-found/not-found-component';
+import { CountryInformationComponent } from './country-information/country-information.component';
 
 @Component({
   selector: 'app-country-page',
-  imports: [ByCapitalPageComponent, NotFoundComponent],
+  imports: [
+    ByCapitalPageComponent,
+    NotFoundComponent,
+    CountryInformationComponent,
+  ],
   templateUrl: './country-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
