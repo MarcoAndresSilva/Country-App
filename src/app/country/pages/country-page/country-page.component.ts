cc4b31f419d6b92c704ca3d0331a18a0fddd0fee
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-06-01 23:49:18 -0400

    fix contry information

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,23 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
-import { ByCapitalPageComponent } from '../by-capital-page/by-capital-page.component';
 import { NotFoundComponent } from '../../../shared/components/not-found/not-found-component';
 import { CountryInformationComponent } from './country-information/country-information.component';
-
 @Component({
   selector: 'app-country-page',
-  imports: [
-    ByCapitalPageComponent,
-    NotFoundComponent,
-    CountryInformationComponent,
-  ],
+  imports: [NotFoundComponent, CountryInformationComponent],
   templateUrl: './country-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CountryPageComponent {
-  countrycode = inject(ActivatedRoute).snapshot.paramMap.get('code') || '';
+  countrycode = inject(ActivatedRoute).snapshot.params['code'];
+  countryService = inject(CountryService);
 
   countryResource = rxResource({
     request: () => ({ code: this.countrycode }),
