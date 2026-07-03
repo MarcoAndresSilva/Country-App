9e7f43467c153bd9bfbd03f3c3545162124c2ce6
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-07-02 23:56:53 -0400

    feat: paises por region

--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,11 +1,21 @@
 import { Component, input } from '@angular/core';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { RESTCountry } from '../../interfaces/rest-countries.interface';
+import { Country } from '../../interfaces/country.interface';
+import { Region } from '../../interfaces/region.type';
 @Component({
   selector: 'app-by-region-page.component',
   imports: [CountryListComponent],
   templateUrl: './by-region-page.component.html',
 })
 export class ByRegionPageComponent {
-  countries = input.required<RESTCountry[]>();
+
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+    'Antarctic',
+  ]
 }
