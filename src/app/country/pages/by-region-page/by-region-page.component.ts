1d4fa8e280389f56b0988a550933ed82927284eb
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-07-07 23:13:41 -0400

    feat: add search by region

--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,14 +1,18 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { RESTCountry } from '../../interfaces/rest-countries.interface';
 import { Country } from '../../interfaces/country.interface';
 import { Region } from '../../interfaces/region.type';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { CountryService } from '../../services/country.service';
+import { of } from 'rxjs';
 @Component({
   selector: 'app-by-region-page.component',
   imports: [CountryListComponent],
   templateUrl: './by-region-page.component.html',
 })
 export class ByRegionPageComponent {
+  countryService = inject(CountryService);
 
   public regions: Region[] = [
     'Africa',
@@ -20,4 +24,13 @@ export class ByRegionPageComponent {
   ]
 
   selectedRegion = signal<Region | null>(null);
+
+  countryResource = rxResource({
+    request: () => ({ region: this.selectedRegion() }),
+    loader: ({ request }) => {
+      if (!request.region) return of([]);
+
+      return this.countryService.searchByRegion(request.region);
+    },
+  });
 }
